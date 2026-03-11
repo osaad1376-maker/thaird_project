86d706421599b0e15e563c040cd6728c43ff45a5
Omar Saad <osaad1376@gmail.com>
2026-03-11 09:54:14 +0200

    Add files via upload

---
+++ Onsite3/script.js
@@ -0,0 +1,76 @@
+let menuItems = [
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food1.png"
+    },
+    {
+        name: "JUMBO CRAB SHRIMP",
+        price: "$24.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food2.png"
+    },
+
+    {
+        name: "KOKTAIL JUICE",
+        price: "$12.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food3.png"
+    },
+    {
+        name: "CAPO STEAK",
+        price: "$60.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food4.png"
+    },
+    {
+        name: "ORGANIC FRUIT SALAD",
+        price: "$8.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food5.png"
+    },
+    {
+        name: "CHEESE PIZZA",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food6.png"
+    },
+    {
+        name: "KOFTA MEAT",
+        price: "$40.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food7.jpeg"
+    },
+    {
+        name: "SPANISH PIES",
+        price: "$14.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food8.jpeg"
+    },
+    {
+        name: "CHEESE TOST",
+        price: "$6.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food9.jpeg"
+
+    },
+    {
+        name: "FRUIT SALAD",
+        price: "$14.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food10.jpeg"
+    },
+    {
+        name: "CHICKEN SHAWARMA",
+        price: "$20.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food11.jpeg"
+    },
+    {
+        name: "MEGA CHEESE PIZZA",
+        price: "$30.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
+        image: "food12.jpeg"
+    }
+] 
